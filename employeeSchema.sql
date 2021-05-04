8b71a8d8cfec8ba76c056e82ec441b01848b82a2
elgalloloco <zreggert@gmail.com>
2021-05-04 18:17:37 -0400

    created schema for the tables in themployee db

---
+++ employeeSchema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE DATABASE employee_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(5,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+)
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "",
+    database: "employee_db",
+  });
+  
+
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
